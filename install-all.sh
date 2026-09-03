95d90d44b701233f2cf7b9eb8bc10d95a04d314d
J Freeman <155204947+jmf-pobox@users.noreply.github.com>
2026-09-03 06:30:29 -0700

    chore: update quarry install SHA to v3.2.1 (#377)

--- install-all.sh
+++ install-all.sh
@@ -57,7 +57,7 @@ banner "biff"
 curl -fsSL "$GH/biff/5b25cf0/install.sh" | sh
 
 banner "quarry"
-curl -fsSL "$GH/quarry/96dac0a/install.sh" | sh
+curl -fsSL "$GH/quarry/935c58d/install.sh" | sh
 
 banner "vox"
 curl -fsSL "$GH/vox/ebaf2f7/install.sh" | sh
